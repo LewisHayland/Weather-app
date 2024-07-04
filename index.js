9c22c617c0012247ce6a5e11f87609498636c3e9
LewisHayland <lhayland@gmail.com>
2024-07-04 17:01:16 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function getWeather() {
     //Please add your own apikey below for this application to work!!
-    const apiKey = '';
+    const apiKey = '049c55548e954e1557ae34ff836a71f3';
     const city = document.getElementById('city').value;
 
     if (!city) {
